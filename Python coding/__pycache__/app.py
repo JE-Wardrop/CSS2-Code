561f95a04a49bc7db505902bdd217036ca8f8442
JE-Wardrop <3110drop@gmail.com>
2026-03-25 18:23:18 +1100

    testing on another machine

---
+++ Python coding/__pycache__/app.py
@@ -0,0 +1,7 @@
+import pandas as pd
+from matplotlib import pyplot
+from plotdata import roll_num, marks
+import plotdata
+
+pyplot.plot(roll_num, marks)
+pyplot.show()
---
+++ Python coding/__pycache__/plotdata.py
@@ -0,0 +1,5 @@
+# x-axis values
+roll_num = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# y-axis values
+marks = [55,75,96,75,36,45,87,99,100]
