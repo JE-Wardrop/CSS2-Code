5426dd719f53f318cb4e4bfc159d1b248b5dc037
JE-Wardrop <3110drop@gmail.com>
2026-03-25 18:32:15 +1100

    data mining beginning code. this is just a draft for the beginning, it will not be final. It also does not use a search relevant for OUR uses. We will chnage this later on

--- Python coding/__pycache__/app.py
+++ Python coding/__pycache__/app.py
@@ -1,7 +1,31 @@
-import pandas as pd
-from matplotlib import pyplot
-from plotdata import roll_num, marks
-import plotdata
+# tut from: https://medium.com/@darshankhandelwal12/scrape-google-scholar-using-python-3f35a3a6597b
+import requests
+from bs4 import BeautifulSoup
 
-pyplot.plot(roll_num, marks)
-pyplot.show()
+def getScholarProfiles():
+    try:
+        url = "https://scholar.google.com/citations?hl=en&view_op=search_authors&mauthors=Quantum+Physics"
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36"
+        }
+        response = requests.get(url, headers=headers)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        
+        scholar_profiles = []
+        for el in soup.select('.gsc_1usr'):
+            profile = {
+                'name': el.select_one('.gs_ai_name').get_text(),
+                'name_link': 'https://scholar.google.com' + el.select_one('.gs_ai_name a')['href'],
+                'position': el.select_one('.gs_ai_aff').get_text(),
+                'email': el.select_one('.gs_ai_eml').get_text(),
+                'departments': el.select_one('.gs_ai_int').get_text(),
+                'cited_by_count': el.select_one('.gs_ai_cby').get_text().split(' ')[2]
+            }
+            scholar_profiles.append({k: v for k, v in profile.items() if v})
+        
+        print(scholar_profiles)
+    except Exception as e:
+        print(e)
+
+getScholarProfiles()
+    
